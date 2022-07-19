787598df8551b99a3e03e3f1532e9df064ceca49
healygi <healygi@tcd.ie>
2022-07-19 14:29:36 +0000

    fixed bug on fb login

--- healyhealth/settings.py
+++ healyhealth/settings.py
@@ -20,14 +20,9 @@ if os.path.isfile("env.py"):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
@@ -72,21 +67,16 @@ SOCIALACCOUNT_PROVIDERS = {'facebook': {'METHOD': 'oauth2',
 # ACCOUNT_UNIQUE_EMAIL = True
 # ACCOUNT_USERNAME_REQUIRED = False
 
-
-# AUTHENTICATION_BACKENDS = (
-#     "allauth.account.auth_backends.AuthenticationBackend",
-# )
-
 SITE_ID = 1
 ACCOUNT_EMAIL_VERIFICATION = "none"
 LOGIN_REDIRECT_URL = "home"
 ACCOUNT_LOGOUT_ON_GET = True
 
-# Add the 'allauth' backend to AUTHENTICATION_BACKEND and keep default ModelBackend
-AUTHENTICATION_BACKENDS = [ 'django.contrib.auth.backends.ModelBackend', 
-'allauth.account.auth_backends.AuthenticationBackend'] 
-# EMAIL_BACKEND so allauth can proceed to send confirmation emails
-# ONLY for development/testing use console 
+AUTHENTICATION_BACKENDS = [ 
+'django.contrib.auth.backends.ModelBackend', 
+'allauth.account.auth_backends.AuthenticationBackend', 
+]
+
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 
 # Custom allauth settings
@@ -136,7 +126,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.contrib.sites.models.Site.'
             ],
         },
     },
