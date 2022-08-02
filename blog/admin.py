6313ca529326098ec04083251f7ff90391c063fb
healygi <healygi@tcd.ie>
2022-08-02 16:55:35 +0000

    validate all files through validator and fix bugs- all files pass 100% through validators

--- blog/admin.py
+++ blog/admin.py
@@ -21,5 +21,7 @@ class CommentAdmin(admin.ModelAdmin):
     search_fields = ['name', 'email', 'body']
     actions = ['approve_comments']
 
+
+
     def approve_comments(self, request, queryset):
-        queryset.update(approved=True)
+        queryset.update(approved = True)
