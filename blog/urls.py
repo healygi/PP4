c75f87114f97d8cb5b8a971bbd353d44ca65974e
healygi <healygi@tcd.ie>
2022-08-04 19:04:36 +0000

    attempt to add delete and edit comments functionality- broke code - push before close

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,8 @@
 from . import views 
-# from .views import PostEdit
 from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name = 'home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name = 'post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name = 'post_like'),
-    path('comment/<slug:slug>/update', UpdatePostView.as_view(), name ='update_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,14 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
-from django.views import UpdateView
-# from django.views.generic.edit import UpdateView
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
-class PostList(generic.ListView):
+class PostList(ListView):
     model = Post
     queryset = Post.objects.filter(status = 1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
-
-class UpdatePostView(UpdateView):
-    model = Comment
-    template_name = 'update_post.html'
-    fields = ["body"]
-
-    success_url = "/"
 
 
 class PostDetail(View):
